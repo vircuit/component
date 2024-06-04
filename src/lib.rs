85596d3f556abd8e7e1ab58dc5ddb7cd2cb4e566
weicaixin <weicaixin@qq.com>
2024-06-04 11:46:26 +0800

    add connect

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,21 @@ pub trait Component {
     fn run(&mut self) -> anyhow::Result<()>;
     fn stop(&mut self) -> anyhow::Result<()>;
 }
+
+pub fn connect<SIGNAL, SLOT, ARGS>(signal: SIGNAL, slot: SLOT) -> std::thread::JoinHandle<()>
+where
+    SIGNAL: FnOnce(std::sync::mpsc::SyncSender<ARGS>) + Send + 'static,
+    SLOT: Fn(&ARGS) -> anyhow::Result<()> + Send + 'static,
+    ARGS: Send + 'static,
+{
+    let (tx, rx) = std::sync::mpsc::sync_channel(100);
+    signal(tx);
+
+    std::thread::spawn(move || {
+        for args in rx {
+            if let Err(e) = slot(&args) {
+                eprintln!("Error: {:?}", e);
+            }
+        }
+    })
+}
